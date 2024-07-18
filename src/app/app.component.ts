a262dbfdcbbb29b72c97a24a086f5b9ab6c0f4bd
fayssaldev <elmfayssal@gmail.com>
2024-07-18 15:24:36 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfilComponent } from './profil/profil.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            RouterModule,
+            HeaderComponent,
+            SidebarComponent,
+            MainContentComponent,
+            FooterComponent,
+            ProfilComponent,
+            LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LoginComponent } from './login/login.component';
+import { ProfilComponent } from './profil/profil.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+    { path: 'main', component: MainContentComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'profil', component: ProfilComponent },
+
+
+];
